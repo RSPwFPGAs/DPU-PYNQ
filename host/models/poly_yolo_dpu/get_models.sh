6a3a24810c80ebeb2bebed47c13bb1bd704e18d7
RSPwFPGAs <RSPwFPGAs@github.com>
2020-10-12 18:35:58 +0800

    Added 3 model compilation projects.

---
+++ host/models/poly_yolo_dpu/get_models.sh
@@ -0,0 +1,11 @@
+wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/poly_yolo.py
+wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/yolo_anchors.txt
+wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/yolo_classes.txt
+ls 
+ls ..
+mkdir models
+cd models/
+wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/models/poly_yolo.h5
+wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/models/poly_yolo_lite.h5
+cd ..
+
---
+++ host/models/yolov3_dpu/compile_yolov3_ultra96.sh
@@ -0,0 +1,1 @@
+../../compile.sh Ultra96 tf_yolov3_voc_416_416_65.63G_1.1
