2ea46e99daa66e54c1b4e02ab6592506c477600d
RSPwFPGAs <RSPwFPGAs@github.com>
2020-10-13 06:51:33 +0800

    Updated PolyYolo model downloading script.

--- host/models/poly_yolo_dpu/get_models.sh
+++ host/models/poly_yolo_dpu/get_models.sh
@@ -1,11 +1,18 @@
-wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/poly_yolo.py
-wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/yolo_anchors.txt
-wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/yolo_classes.txt
-ls 
-ls ..
-mkdir models
-cd models/
-wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/models/poly_yolo.h5
-wget https://gitlab.com/irafm-ai/poly-yolo/-/blob/master/poly_yolo/models/poly_yolo_lite.h5
-cd ..
+if [ -f "poly_yolo.py" ]; then
+	echo "Model definition exists!"
+else
+	wget https://gitlab.com/irafm-ai/poly-yolo/-/raw/master/poly_yolo/poly_yolo.py
+	wget https://gitlab.com/irafm-ai/poly-yolo/-/raw/master/poly_yolo/yolo_anchors.txt
+	wget https://gitlab.com/irafm-ai/poly-yolo/-/raw/master/poly_yolo/yolo_classes.txt
+fi
+
+if [ -d "models" ]; then
+	echo "Model parameter exists!"
+else
+	mkdir models
+	cd models/
+	wget https://gitlab.com/irafm-ai/poly-yolo/-/raw/master/poly_yolo/models/poly_yolo.h5
+	wget https://gitlab.com/irafm-ai/poly-yolo/-/raw/master/poly_yolo/models/poly_yolo_lite.h5
+	cd ..
+fi
 
